cd3bd5df4cac97d7aa92c276f392a8c9eb6f821b
earowang <earo.wang@gmail.com>
2016-05-24 20:37:05 +1000

    computate and update cognostics for pedestrians sensors

---
+++ data-raw/cognostics.R
@@ -0,0 +1,63 @@
+# devtools::install_github("earowang/tscognostics")
+library(dplyr)
+library(jsonlite)
+library(lubridate)
+library(tscognostics)
+
+## Pull data from the web
+limit <- 1453000 # all the up-to-date records needs to be retrieved
+web_add <- "https://data.melbourne.vic.gov.au/resource/mxb8-wn4w.json?" 
+ped_url <- paste0(web_add, "$limit=", limit)
+pedestrians <- fromJSON(ped_url) # without api token
+pedestrians <- tbl_df(pedestrians)
+colnames(pedestrians) <- c("DateTime", "Counts", "SensorID", "SensorName")
+pedestrians <- pedestrians %>%
+  mutate(DateTime = as.POSIXct(strptime(pedestrians$DateTime, "%d-%b-%Y %H:%M")),
+         Counts = as.integer(Counts), SensorID = factor(SensorID))
+
+## Look at data from 2013 Jan to 2016 Jan
+pedestrians <- pedestrians %>%
+  filter(DateTime >= as.POSIXct("2013-01-01 00:00:00") &
+         DateTime <= as.POSIXct("2016-01-31 23:00:00"))
+
+## Data cleaning
+pedestrians <- pedestrians %>%
+  mutate(SensorName = ifelse(SensorName == "Lygon St (West)",
+                             "Lygon Street (West)", 
+                             SensorName))
+pedestrians <- pedestrians %>%
+  mutate(SensorName = ifelse(SensorName == "Lonsdale St (South)",
+                             "Lonsdale Street (South)", 
+                             SensorName))
+pedestrians <- pedestrians %>%
+  mutate(SensorName = ifelse(SensorName == "Bourke St-Russel St (West)",
+                             "Bourke St-Russell St (West)", 
+                             SensorName))
+
+## Adding new variables
+names(pedestrians) <- sub("^Sensor", "", names(pedestrians))
+pedestrians <- pedestrians %>% 
+  mutate(Year = year(DateTime)) %>%
+  mutate(Month = month(DateTime)) %>%
+  mutate(Day = day(DateTime)) %>%
+  mutate(Wday = wday(DateTime)) %>%
+  mutate(Weekend = ifelse(Wday %in% c(6, 7), "Weekend", "Weekday")) %>%
+  mutate(Hour = hour(DateTime))
+
+## Turn the df to time series
+dates <- sort(unique(pedestrians$DateTime))
+ids <- unique(pedestrians$ID)
+num_ids <- length(ids)
+ts_pedestrians <- matrix(NA, nrow = length(dates), ncol = num_ids)
+for (i in 1L:num_ids) {
+  tmp <- pedestrians %>% filter(ID == i)
+  j <- is.element(dates, tmp$DateTime)
+  ts_pedestrians[j, i] <- tmp$Counts
+}
+ts_pedestrians <- ts(ts_pedestrians, frequency = 24)
+
+## Computate cognostics
+cog <- tsmeasures(ts_pedestrians, width = 48) # 7*24 = 2 days window
+# cog <- read_csv("data-raw/pedestrian-cognostics.csv")
+names(cog) <- c("ID", names(cog)[-1])
+devtools::use_data(cog, overwrite = TRUE)
